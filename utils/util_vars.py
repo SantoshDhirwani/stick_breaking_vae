0ad8ac62fca6bbaff47ca2db32b193a3fa2f27fb
TheDeclineOfTheAmericanMiddleClass <am5098@nyu.edu>
2021-01-27 19:24:46 +0100

    fixing util variables

--- utils/util_vars.py
+++ utils/util_vars.py
@@ -6,7 +6,6 @@ from torch import nn
 from torchvision import transforms
 
 # arg defaults in https://github.com/enalisnick/stick-breaking_dgms/blob/master/run_gauss_VAE_experiments.py
-
 seed = 1234
 torch.set_rng_state(torch.manual_seed(seed).get_state())
 batch_size = 100
@@ -16,16 +15,16 @@ learning_rate = 3e-4
 lookahead = 30
 print_interval = 10
 n_train_epochs = 10
-max_epoch = 2000
+max_epoch = 2000  # TODO: implement early stopping
 init_weight_mean_var = (0, .001)
 prior_mu = torch.Tensor([0.])
 prior_sigma = torch.Tensor([1.])
 prior_shape_alpha = torch.Tensor([1.])
 prior_shape_beta = concentration_alpha0 = torch.Tensor([5.])
 activation = nn.ReLU()
-train_valid_test_splits = (5000, 5000, 10000)
+train_valid_test_splits = (45000, 5000, 10000)
 dataloader_kwargs = {}
-download_needed = not os.path.exists('../MNIST')
+download_needed = not os.path.exists('./MNIST')
 
 # use GPU, if available
 CUDA = torch.cuda.is_available()
