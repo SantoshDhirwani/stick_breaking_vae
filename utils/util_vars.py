34d8cf655a46d577b30fa9f43da9537e9540171f
TheDeclineOfTheAmericanMiddleClass <am5098@nyu.edu>
2021-02-23 09:32:58 +0100

    lower lr, more monte carlo samples, fewer train epochs

--- utils/util_vars.py
+++ utils/util_vars.py
@@ -11,22 +11,25 @@ torch.set_rng_state(torch.manual_seed(seed).get_state())
 batch_size = 100
 latent_ndims = 50
 hidden_ndims = 500
-learning_rate = 3e-4
+learning_rate = 1e-4
 lookahead = 30
 print_interval = 10
-n_train_epochs = 2000
+n_train_epochs = 1500
 init_weight_mean_var = (0, .001)
 prior_mu = torch.Tensor([0.])
 prior_sigma = torch.Tensor([1.])
-prior_shape_alpha = torch.Tensor([1.])
-prior_shape_beta = concentration_alpha0 = torch.Tensor([5.])
+prior_alpha = torch.Tensor([1.])
+prior_beta = concentration_alpha0 = torch.Tensor([5.])
 uniform_low = torch.Tensor([.01])
 uniform_high = torch.Tensor([.99])
 activation = nn.ReLU()
 train_valid_test_splits = (45000, 5000, 10000)
-n_monte_carlo_samples = 1
+n_monte_carlo_samples = 10
+n_random_samples = 16
 dataloader_kwargs = {}
 download_needed = not os.path.exists('./MNIST')
+model_path = 'trained_models'
+checkpoint_path = None  # specify path to continue training a model
 
 # use GPU, if available
 CUDA = torch.cuda.is_available()
